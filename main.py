adcecc690b8c027f1a30446223bc9b61ed873f14
khurram676 <khurramshabir475@gmail.com>
2024-12-15 16:27:37 -0800

    grading in python

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 marks= int(input("enter yout marks from 1 to 100: "))
 if marks>70:
-    print("good marks: ",marks)
+    print("you are not eiligble: ",marks)
 elif marks<70:
-    print("keep it up")
+    print("you are not eiligble")
  
